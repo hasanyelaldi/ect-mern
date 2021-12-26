097f0feef820130bed943aa462f5822cd7a8aa96
Hasan Yelaldi <hasan.yelaldi@siemens.com>
2021-12-26 21:41:57 +0300

    Cart added

--- ect-client/src/App.js
+++ ect-client/src/App.js
@@ -4,9 +4,10 @@ import Product from './pages/Product'
 import ProductList from './pages/ProductList'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Cart from './pages/Cart'
 
 const App = () => {
-  return <Login/>
+  return <Cart/>
 }
 
 export default App
