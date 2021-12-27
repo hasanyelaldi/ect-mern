ea645adf7352b54152f2a45b11361e6788d902ca
Hasan Yelaldi <hasan.yelaldi@siemens.com>
2021-12-27 21:44:07 +0300

    api init

---
+++ ect-api/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DBConnection succesfull"))
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running");
+})
